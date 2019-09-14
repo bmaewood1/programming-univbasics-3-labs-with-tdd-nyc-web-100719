9f1c4637035a503a1f97a94553838eb2fb446e64
Bailey Wood <woodbmae@gmail.com>
2019-09-14 18:57:15 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,12 +1,12 @@
 # Add your variables here
 
-def first_number(number)
-  puts number
-end
+first_number = 2 
 
 def get_variable_from_file
   first_number
 end
 
-  
+get_variable_from_file
+
+
   
