029be1cf941d33011c49d63fa0649d2571f3e5ff
Bailey Wood <woodbmae@gmail.com>
2019-09-14 19:01:35 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,8 +1,14 @@
 # Add your variables here
 
+first_number = 2 
 
+second_number = 1 
 
+sum = first_number+second_number
 
+difference = first_number-secoond_number
 
+product = first_number*second_number
 
-  
+quotient = first_number*second_number
+
